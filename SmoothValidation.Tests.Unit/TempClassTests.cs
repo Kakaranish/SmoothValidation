4bfb2251f9e9ffefa340ecdb7c8299284e8cbad7
Kakaranish <stasiekgruz@gmail.com>
2020-09-25 21:21:44 +0200

    + added empty test project

---
+++ SmoothValidation.Tests.Unit/TempClassTests.cs
@@ -0,0 +1,16 @@
+﻿using FluentAssertions;
+using NUnit.Framework;
+
+namespace SmoothValidation.Tests.Unit
+{
+    [TestFixture]
+    public class TempClassTests
+    {
+        [Test]
+        public void Test1()
+        {
+            var value = true;
+            value.Should().Be(true);
+        }
+    }
+}
